820b9419ee2608e896de6329b3b8c14c75a34865
khalidov <khalidov@haulmont.com>
2016-11-02 18:12:05 +0400

    above impr

--- above.py
+++ above.py
@@ -15,11 +15,11 @@ class Source(Base):
         self.rank = 999
 
     def gather_candidates(self, context):
-        logger.info('line: ' + str(context))
-
         line = context['position'][1]
+        complete_str = context['complete_str']
         candidates = parse_buffer_pattern(
                         reversed(self.vim.call('getline', max([1, line - 20]), line)),
                         context['keyword_patterns'],
                         context['complete_str'])
-        return [{'word': x} for x in candidates]
+        return [{'word': x} for x in candidates if x.startswith(complete_str)]
+
